9d7db2f0b6549ac2c97b8288a909208f1592ecb2
Naufal <nauf1207@mail.ru>
2023-03-23 18:12:53 +0600

    Решение доп.задачи

---
+++ Additional task/Program.cs
@@ -0,0 +1,49 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 32);
+}
+int PositiveRating(int[] array)
+{
+    int count = 0;  
+    for (int i = 0; i < array.Length; i++)
+    {
+      if (array[i] % 2 == 0)
+          count ++;
+    }
+    return count;
+}
+int NegativeRating(int[] array)
+{
+    int count = 0;    
+    for (int i = 0; i < array.Length; i++)
+    {
+      if (array[i] % 2 != 0)      
+      count ++;
+    }
+    return count;
+}
+string GoodStudent(int[] array)
+{    
+    for (int i = 0; i < array.Length; i++)
+    {
+      if (NegativeRating(array) < PositiveRating(array))
+      {
+      Console.WriteLine(array);     
+      return "yes";
+      }
+      
+       
+    }
+return "no";   
+}
+
+Console.Clear();
+Console.Write("Введите кол-во дней в месяце: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Количество четверок = {PositiveRating(array)}");
+Console.WriteLine($"Количество троек = {NegativeRating(array)}");
+Console.WriteLine($"Ударник: {GoodStudent(array)}");
